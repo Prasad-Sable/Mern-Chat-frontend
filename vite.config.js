ff79abdb6fab96fbd832045614a32fb3e0e493cb
Prasad-Sable <prasadsable2000@gmail.com>
2024-07-22 00:04:54 +0530

    two

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
       proxy:{
-        "/api": 'http://localhost:5800',
+        "/api": 'https://pixelchat-gla8.onrender.com',
       }
   },
   plugins: [react()],
