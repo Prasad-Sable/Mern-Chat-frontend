fe97d4045154518ccde1f9cadef9c69e2653c2a0
Prasad-Sable <prasadsable2000@gmail.com>
2024-07-23 00:35:59 +0530

    latest1

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: './src/main.jsx',
-    },
-  },
+
   server:{
       proxy:{
         "/api": 'https://pixelchat-gla8.onrender.com',
